40f56449016f0268601ae87093ed50c9328cc3c7
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2023-12-05 17:07:46 +0100

    update commit

--- src/Es2/MainList.java
+++ src/Es2/MainList.java
@@ -1,12 +1,10 @@
 package Es2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class MainList {
-    public static void main(String[] args) { Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Inserisci un numero causale da 1 a 100");
         int numberChoose = scanner.nextInt();
 
@@ -18,6 +16,9 @@ public class MainList {
     }
 
     private static void crealistaNumeriInvertiti(List<Integer> listaNumeriInvertiti) {
+        Collections.reverse(listaNumeriInvertiti);
+        listaNumeriInvertiti.addAll(new ArrayList<>(listaNumeriInvertiti));
+        System.out.println("Lista numeri invertiti: " + listaNumeriInvertiti);
     }
 
     private static void generaListaCasuale(List<Integer> listaCasual, int numberChoose) {
@@ -26,5 +27,18 @@ public class MainList {
         for (int i = 0; i < numberChoose; i++) {
             listaCasual.add(random.nextInt(101));
         }
-        System.out.println("Lista casuale:" + listaCasual);}
+        System.out.println("Lista casuale:" + listaCasual);
+        numberP(listaCasual);
+        Collections.sort(listaCasual);
+        System.out.println("Lista ordinata" + listaCasual);
+    }
+
+    private static void numberP(List<Integer> listaCasual) {
+        System.out.println("Ecco i numeri pari:");
+        for (int i = 0; i < listaCasual.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(listaCasual.get(i));
+            }
+        }
+    }
 }
